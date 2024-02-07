0d94467262c809a9c68204bedf5123e0ca1203ec
francesco <info.francescobianco@gmail.com>
2024-02-07 19:33:35 +0100

    wip

--- src/main.sh
+++ src/main.sh
@@ -1,3 +1,12 @@
 
 
 main
+
+[ "$1" = "--sync" ] && refresh_interval=0
+
+if [ "$1" = "--auth" ]; then
+  read -r -p "Google Client Id: " client_id
+  read -r -p "Google Client Secret: " client_secret
+  gcalcli --client-id "${client_id}" --client-secret "${client_secret}" list
+  exit 0
+fi
