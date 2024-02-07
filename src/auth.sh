cac7bc5c84ad68ded4a04d1eb5ccff71598b7f7e
francesco <info.francescobianco@gmail.com>
2024-02-07 16:40:24 +0100

    wip

--- src/auth.sh
+++ src/auth.sh
@@ -2,12 +2,25 @@
 google_calendar_script_auth() {
   local access_token_file
   local client_secret_file
+  local last_modified
+  local current_time
 
   access_token_file=$1
   client_secret_file=$2
 
-  [ ! -f "${access_token_file}" ] && google_calendar_script_get_access_token "${access_token_file}" "${client_secret_file}"
+  if [ ! -f "${access_token_file}" ]; then
+    google_calendar_script_get_access_token "${access_token_file}" "${client_secret_file}"
+  fi
 
+  last_modified=$(stat -c %Y "${access_token_file}")
+  current_time=$(date +%s)
+  expiring_time=$((current_time - last_modified))
+
+  echo "$expiring_time"
+
+  if [ "$expiring_time" -gt "1000" ]; then
+    google_calendar_script_refresh_access_token "${access_token_file}" "${client_secret_file}"
+  fi
 }
 
 google_calendar_script_get_access_token() {
@@ -55,4 +68,32 @@ google_calendar_script_get_access_token() {
   echo
   echo "Authorization complete. Access token saved to '${access_token_file}'"
   echo "${response}" > "${access_token_file}"
+}
+
+google_calendar_script_refresh_access_token() {
+  local access_token_file
+  local client_secret_file
+  local client_id
+  local client_secret
+  local refresh_token_url
+  local refresh_token
+
+  access_token_file=$1
+  client_secret_file=$2
+
+  refresh_token_url="https://oauth2.googleapis.com/token"
+
+  client_id=$(sed -n 's/.*"client_id":"\(.*\)".*/\1/p' "${client_secret_file}" | cut -d '"' -f 1)
+  client_secret=$(sed -n 's/.*"client_secret":"\(.*\)".*/\1/p' "${client_secret_file}" | cut -d '"' -f 1)
+  refresh_token=$(sed -n 's/.*"refresh_token": *"\(.*\)".*/\1/p' "${access_token_file}" | cut -d '"' -f 1)
+
+  access_token=$(curl -s -X POST \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "client_id=$client_id" \
+     -d "client_secret=$client_secret" \
+     -d "refresh_token=$refresh_token" \
+     -d "grant_type=refresh_token" \
+     "$refresh_token_url" | sed 's#{#{\n  "refresh_token": "'"${refresh_token}"'",#')
+
+  echo "${access_token}" > "${access_token_file}"
 }
--- tests/auth-test.sh
+++ tests/auth-test.sh
@@ -5,6 +5,6 @@ source src/auth.sh
 access_token_file="tests/tmp/access_token.json"
 client_secret_file="${HOME}/.google/client_secret.json"
 
-rm -fr "${access_token_file}"
+#rm -fr "${access_token_file}"
 
 google_calendar_script_auth "${access_token_file}" "${client_secret_file}"
