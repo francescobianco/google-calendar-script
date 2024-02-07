c527bd30c312265ba40d93d176dcc5c01620a4e1
francesco <info.francescobianco@gmail.com>
2024-02-07 19:41:46 +0100

    wip

--- src/main.sh
+++ src/main.sh
@@ -1,12 +1,14 @@
 
+module auth
+module events
 
-main
+main() {
+  [ "$1" = "--sync" ] && refresh_interval=0
 
-[ "$1" = "--sync" ] && refresh_interval=0
-
-if [ "$1" = "--auth" ]; then
-  read -r -p "Google Client Id: " client_id
-  read -r -p "Google Client Secret: " client_secret
-  gcalcli --client-id "${client_id}" --client-secret "${client_secret}" list
-  exit 0
-fi
+  if [ "$1" = "--auth" ]; then
+    read -r -p "Google Client Id: " client_id
+    read -r -p "Google Client Secret: " client_secret
+    gcalcli --client-id "${client_id}" --client-secret "${client_secret}" list
+    exit 0
+  fi
+}
